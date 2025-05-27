67e89504f5ec60bf4bbf022f03a81685d2b582c6
cleverstrone <35955092+cleverstrone@users.noreply.github.com>
2025-05-27 14:30:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import os
 
 def get_weather(city):
-    api_key = os.environ.get("WEATHER_API_KEY")  # Uses environment variable
+    api_key = os.environ.get("Weather_api")  # Uses environment variable
     if not api_key:
         raise Exception("API key not found in environment variables.")
 
